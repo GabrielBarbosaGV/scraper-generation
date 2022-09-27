dadfaf7781b594e73736a4ccc9797016751a95cd
GabrielBarbosaGV <gabrielbgasparv@gmail.com>
2022-09-27 19:28:04 +0100

    Using fixed jest.fn() instead of generated function

--- src/libs/completion/completion.test.ts
+++ src/libs/completion/completion.test.ts
@@ -3,21 +3,23 @@ import { completionsForAllDescriptions } from "./completion";
 
 describe('completionsForAllDescriptions', () => {
     test('awaits all completions and returns them', () => {
-        fc.assert(
-            fc.property(
+        return fc.assert(
+            fc.asyncProperty(
                 fc.array(
                     fc.string(),
                     { maxLength: 50 }
-                ).chain(ss => fc.tuple(fc.constant(ss), fc.func(fc.string()))),
+                ),
 
-                ([ss, immediateCompleter]) => {
-                    const promiseCompleter = (s: string): Promise<string> => new Promise((resolve) => resolve(immediateCompleter(s)));
+                async (ss) => {
+                    const immediateCompleter = jest.fn().mockImplementation(s => `${s}${s}`);
 
-                    const allCompletions = completionsForAllDescriptions(ss, promiseCompleter);
+                    const promiseCompleter = async (s: string) => immediateCompleter(s);
 
-                    const promise = allCompletions.then(allS => allS.map(s => ss.includes(s)).reduce((a, b) => a && b, true));
+                    const allCompletions = await completionsForAllDescriptions(ss, promiseCompleter);
 
-                    expect(promise).resolves.toBeTruthy();
+                    const mappedSs = ss.map(immediateCompleter);
+
+                    expect(allCompletions).toEqual(mappedSs);
                 }
             )
         );
